672e37ca7367958190cde3531e7af92ffedd9c7a
kimia farzaneh <kymyafarzaneh@gmail.com>
2022-09-07 16:39:04 +0430

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import Card from "./components/card";
-import "./App.css"
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const profileData = async () => {
+      let userData;
+      try {
+        const res = await axios.get("https://randomuser.me/api/?results=10");
+        userData = res.data;
+      } catch (error) {
+        console.log(error);
+      }
+
+      setUsers(userData.results);
+    };
+    profileData();
+  }, []);
+
   return (
-    <div className="container" >
-    <Card/>
-    <Card/>
-    <Card/>
+    <div className="container">
+      {users.map((user) => (
+        <Card key={user.id.value} userData={user} />
+      ))}
     </div>
   );
 }
 
 export default App;
+
--- src/components/card.js
+++ src/components/card.js
@@ -1,38 +1,14 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
 import "./card.css";
 
-const Card = () => {
-  const [profileName, setProfileName] = useState("");
-  const [profileCell, setProfileCell] = useState("");
-  const [profileImage, setProfileImage] = useState("");
-  const [profileEmail, setProfileEmail] = useState("");
+const Card = ( {userData}) => {
 
-  const profileData = async () => {
-    try {
-      const res = await axios.get("https://randomuser.me/api/");
-      setProfileCell(res.data.results[0].cell);
-      setProfileEmail(res.data.results[0].email);
-      setProfileImage(res.data.results[0].picture.large);
-      setProfileName(
-        `${res.data.results[0].name.first} ${res.data.results[0].name.last}`
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    profileData();
-  }, []);
 
   return (
     <div >
       <div className="card">
-        <img src={profileImage} style={{ width: "100%" }} alt="profile"/>
-        <h1>{profileName}</h1>
-        <p className="title">{profileEmail}</p>
-        <p>{profileCell}</p>
+        { <img src={userData.picture.medium} style={{ width: "100%" }} alt="profile"/> }
+        <h1>{`${userData.name.first} ${userData.name.last}`}</h1>
+        <p className="title">{userData.email}</p>
         <p>
           <button>Contact</button>
         </p>
